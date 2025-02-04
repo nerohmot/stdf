f50af272659bec949a2572d9aecac818eedde43f
Tom Hören <horen.tom@gmail.com>
2025-02-04 19:01:58 +0100

    fix the build.rs file

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ use std::path::PathBuf;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let include_dir = PathBuf::from(crate_dir).join("include");
+    let include_dir = PathBuf::from(crate_dir.clone()).join("include");
 
     // Generate C header
     let c_header = include_dir.join("stdf.h");
@@ -15,8 +15,10 @@ fn main() {
 
     // Generate C++ header
     let cpp_header = include_dir.join("stdf.hpp");
-    cbindgen::generate(crate_dir)
-        .expect("Unable to generate C++ bindings")
+    cbindgen::Builder::new()
+        .with_crate(crate_dir.clone())
         .with_language(cbindgen::Language::Cxx)
+        .generate()
+        .expect("Unable to generate C++ bindings")
         .write_to_file(cpp_header);
 }
