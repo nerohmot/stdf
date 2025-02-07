8b872583df1c6faf912d354511fbffc98bb7299b
Tom Hören <horen.tom@gmail.com>
2025-02-07 12:12:53 +0100

    build.rs trial

---
+++ src/bin/mybuild.rs
@@ -0,0 +1,43 @@
+fn main() {
+    let env_vars = [
+        "CARGO_MANIFEST_DIR",
+        "OUT_DIR",
+        "TARGET",
+        "HOST",
+        "NUM_JOBS",
+        "OPT_LEVEL",
+        "DEBUG",
+        "PROFILE",
+        "CARGO_PKG_VERSION",
+        "CARGO_PKG_VERSION_MAJOR",
+        "CARGO_PKG_VERSION_MINOR",
+        "CARGO_PKG_VERSION_PATCH",
+        "CARGO_PKG_VERSION_PRE",
+        "CARGO_PKG_NAME",
+        "CARGO_PKG_DESCRIPTION",
+        "CARGO_PKG_HOMEPAGE",
+        "CARGO_PKG_REPOSITORY",
+        "CARGO_PKG_LICENSE",
+        "CARGO_PKG_AUTHORS",
+    ];
+
+    for &var in &env_vars {
+        if let Ok(value) = std::env::var(var) {
+            println!("{}: {}", var, value);
+        }
+    }
+
+    // Print all CARGO_FEATURE_* variables
+    for (key, value) in std::env::vars() {
+        if key.starts_with("CARGO_FEATURE_") {
+            println!("{}: {}", key, value);
+        }
+    }
+
+    // Print all CARGO_CFG_* variables
+    for (key, value) in std::env::vars() {
+        if key.starts_with("CARGO_CFG_") {
+            println!("{}: {}", key, value);
+        }
+    }
+}
